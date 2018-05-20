692e9c5f187779a89a08895b82169491694d8d55
chizhikova_aa <chizhikovaaa@mdi.ru>
2018-05-20 19:13:40 +0400

    #4 Enlarge tests

--- src/test/java/com/mls/users/TestUserService.java
+++ src/test/java/com/mls/users/TestUserService.java
@@ -13,10 +13,13 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.json.JacksonTester;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.security.authentication.encoding.Md5PasswordEncoder;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.ResultMatcher;
 import org.springframework.web.context.WebApplicationContext;
 
 import java.util.UUID;
@@ -24,6 +27,7 @@ import java.util.UUID;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
 
@@ -60,73 +64,74 @@ public class TestUserService {
 	 */
 	private final static String REQUEST_PATH = "/v1/users";
 
+	/**
+	 * User to test
+	 */
+	private User user;
+
 
 	/**
-	 * Setup before tests
+	 * Setup before test
+	 * </p>
 	 * @throws Exception on fail
 	 */
 	@Before
 	public void setup() throws Exception {
+		//setup spring mock context
 		this.mockMvc = webAppContextSetup(webApplicationContext).build();
+
+		//init JacksonTester json mapping objects
 		ObjectMapper objectMapper = new ObjectMapper();
 		JacksonTester.initFields(this, objectMapper);
+
+		//init user
+		user = new User();
+		user.setId(UUID.randomUUID());
+		user.setUserName("user_" + System.currentTimeMillis());
+		user.setAddress("address" + System.currentTimeMillis());
+		user.setEmail("test_888@mail.com");
+		user.setHidden(false);
+		user.setPhone("+12223333333");
+
+		//password suggested to be encoded (see spec. on swagger.ylm)
+		user.setPassword(new Md5PasswordEncoder()
+				.encodePassword("12345", Configuration.PASSWORD_SALT + user.getUserName()));
 	}
 
 
 	/**
-	 * Common test
+	 * Test all operations on User
+	 * </p>
 	 * @throws Exception on fail
 	 */
 	@Test
 	public void testUserServiceControllers() throws Exception {
 
-		UUID id = UUID.randomUUID();
+		//Test user CREATE operation ---
+		createUser(user, status().isCreated());
 
-		String random = String.valueOf(Math.random()).substring(2, 13);
-		User user = new User();
-		user.setId(id);
-		user.setUserName("user_" + random);
-		user.setAddress("address" + random);
-		user.setEmail("test_888@mail.com");
-		user.setHidden(false);
-		user.setPhone("+" + random);
-
-		//password must be encoded (see spec. on swgger.yalm)
-		user.setPassword(new Md5PasswordEncoder()
-				.encodePassword("12345", Configuration.PASSWORD_SALT + random));
-
-		//Test user CREATE operation
-		mockMvc.perform(post(REQUEST_PATH, user)
-				.contentType(MediaType.APPLICATION_JSON_UTF8)
-				.content(userJson.write(user).getJson()))
-				.andExpect(status().isCreated())
-				.andDo(print());
 
 		//Test user GET operation
-		String getPath = REQUEST_PATH + "/" + id;
-		mockMvc.perform(get(getPath))
-				.andExpect(status().isOk())
-				.andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
-				.andDo(print());
+		getUser(String.valueOf(user.getId()), status().isOk());  //Success
+		getUser("ABC", status().isBadRequest());             //Invalid argument
+		getUser(String.valueOf(UUID.randomUUID()), status().isNotFound()); //Nonexistent user
 
-		//Test user UPDATE operation
+
+		//Test user UPDATE operation ---
 		JSONObject data = new JSONObject();
-		data.put("userName", "updated_" + random);
-		data.put("address", "updated_" + random);
+		data.put("userName", "updated_" + System.currentTimeMillis());
+		data.put("address", "updated_" + System.currentTimeMillis());
 
-		String updatePath = REQUEST_PATH + "/" + id;
-		mockMvc.perform(put(updatePath)
-				.contentType(MediaType.APPLICATION_JSON_UTF8)
-				.content(String.valueOf(data)))
-				.andExpect(status().isOk())
-				.andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
-				.andDo(print());
+		updateUser(String.valueOf(user.getId()), data, status().isOk()); //Success
+		updateUser("ABC", data, status().isBadRequest());            //Invalid argument
+		updateUser(String.valueOf(UUID.randomUUID()), data, status().isNotFound()); //Nonexistent user
 
-		//Test user DELETE operation
-		String deletePath = REQUEST_PATH + "/" + id;
-		mockMvc.perform(delete(deletePath))
-				.andExpect(status().isOk())
-				.andDo(print());
+
+		//Test user DELETE operation ---
+		deleteUser(String.valueOf(user.getId()), status().isOk()); //Success
+		deleteUser("ABC", status().isBadRequest());            //Invalid argument
+		deleteUser(String.valueOf(user.getId()), status().isNotFound());//Nonexistent user
+
 	}
 
 
@@ -135,41 +140,9 @@ public class TestUserService {
 	 * @throws Exception on fail
 	 */
 	@Test
-	@Ignore
+	@Ignore //comment to test
 	public void testCreateUser() throws Exception {
-
-		UUID id = UUID.randomUUID();
-		String random = String.valueOf(Math.random()).substring(2, 13);
-		User user = new User();
-		user.setId(id);
-		user.setUserName("user_" + random);
-		user.setAddress("address" + random);
-		user.setEmail("test_888@mail.com");
-		user.setHidden(false);
-		user.setPhone("+" + random);
-
-		//here is just an example to remind not to store not encoded passwords
-		user.setPassword(new Md5PasswordEncoder()
-				.encodePassword("12345", Configuration.PASSWORD_SALT));
-
-		mockMvc.perform(post(REQUEST_PATH, user)
-				.contentType(MediaType.APPLICATION_JSON_UTF8)
-				.content(userJson.write(user).getJson()))
-				.andExpect(status().isCreated())
-				.andDo(print());
-	}
-
-	/**
-	 * Test GET ALL operation separately
-	 * @throws Exception on fail
-	 */
-	@Test
-	@Ignore
-	public void testGetUsers() throws Exception {
-		mockMvc.perform(get(REQUEST_PATH))
-				.andExpect(status().isOk())
-				.andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
-				.andDo(print());
+		createUser(user, status().isCreated());
 	}
 
 	/**
@@ -177,12 +150,26 @@ public class TestUserService {
 	 * @throws Exception on fail
 	 */
 	@Test
-	@Ignore
+	@Ignore //comment to test
 	public void testGetUserById() throws Exception {
 
+		//here must be an identifier of existing, not disabled user
 		String id = "88492474-f594-4b08-84a0-bff40f9fd9cb";
-		String getPath = REQUEST_PATH + "/" + id;
-		mockMvc.perform(get(getPath))
+
+		getUser(id, status().isOk()); //Success
+		getUser("ABC", status().isBadRequest()); //Invalid argument
+		getUser(String.valueOf(UUID.randomUUID()), status().isNotFound()); //Nonexistent user
+
+	}
+
+	/**
+	 * Test GET ALL operation separately
+	 * @throws Exception on fail
+	 */
+	@Test
+	@Ignore //comment to test
+	public void testGetUsers() throws Exception {
+		mockMvc.perform(get(REQUEST_PATH))
 				.andExpect(status().isOk())
 				.andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
 				.andDo(print());
@@ -194,36 +181,113 @@ public class TestUserService {
 	 * @throws Exception on fail
 	 */
 	@Test
-	@Ignore
+	@Ignore //comment to test
 	public void testUpdateUser() throws Exception {
-
+		//here must be an identifier of existing, not disabled user
 		String id = "88492474-f594-4b08-84a0-bff40f9fd9cb";
 
 		JSONObject data = new JSONObject();
 		data.put("userName", "updated_" + System.currentTimeMillis());
-		data.put("address", "address_" + System.currentTimeMillis());
+		data.put("address", "updated_" + System.currentTimeMillis());
 
-		String updatePath = REQUEST_PATH + "/" + id;
-		mockMvc.perform(put(updatePath)
-				.contentType(MediaType.APPLICATION_JSON_UTF8)
-				.content(String.valueOf(data)))
-				.andExpect(status().isOk())
-				.andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
-				.andDo(print());
+		updateUser(id, data, status().isOk()); //Expect success
+		updateUser(String.valueOf(UUID.randomUUID()), data, status().isBadRequest()); //Nonexistent user
 	}
 
 	/**
 	 * Test DELETE operation separately
+	 * </p>
 	 * @throws Exception on fail
 	 */
 	@Test
-	@Ignore
+	@Ignore //comment to test
 	public void testDeleteUser() throws Exception {
-
+		//here must be an identifier of existing, not disabled user
 		String id = "88492474-f594-4b08-84a0-bff40f9fd9cb";
+
+		deleteUser(id, status().isOk());                           //Success
+		deleteUser("ABC", status().isBadRequest());            //Invalid argument
+		deleteUser(String.valueOf(user.getId()), status().isNotFound());//Nonexistent user
+	}
+
+
+	/**
+	 * CREATE operation on User
+	 * </p>
+	 * @param user   - user
+	 * @param status - expected http status
+	 * @throws Exception on fail
+	 */
+	private void createUser(User user, ResultMatcher status) throws Exception {
+		mockMvc.perform(post(REQUEST_PATH, user)
+				.contentType(MediaType.APPLICATION_JSON_UTF8)
+				.content(userJson.write(user).getJson()))
+				.andExpect(status)
+				.andDo(print());
+	}
+
+	/**
+	 * GET operation on User by user identifier
+	 * </p>
+	 * @param id     - user identifier
+	 * @param status - expected http status
+	 * @throws Exception on fail
+	 */
+	private void getUser(String id, ResultMatcher status) throws Exception {
+		String getPath = REQUEST_PATH + "/" + id;
+		ResultActions getOperation = mockMvc.perform(get(getPath))
+				.andExpect(status)
+				.andDo(print());
+
+		//if OK, check content type
+		if (getOperation.andReturn().getResponse().getStatus() == HttpStatus.OK.value()) {
+			getOperation.andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8));
+		}
+	}
+
+	/**
+	 * UPDATE operation on User by user identifier
+	 * </p>
+	 * @param id     - user identifier
+	 * @param fields - user fields with values to update in json format
+	 * @param status - expected http status
+	 * @throws Exception on fails
+	 */
+	private void updateUser(String id, JSONObject fields, ResultMatcher status) throws Exception {
+
+		String updatePath = REQUEST_PATH + "/" + id;
+		ResultActions updateOperation = mockMvc.perform(put(updatePath)
+				.contentType(MediaType.APPLICATION_JSON_UTF8)
+				.content(String.valueOf(fields)))
+				.andExpect(status)
+				.andDo(print());
+
+		//if OK, check content type and updated fields
+		if (updateOperation.andReturn().getResponse().getStatus() == HttpStatus.OK.value()) {
+			updateOperation.andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8));
+			fields.keys().forEachRemaining(n -> {
+						try {
+							String key = String.valueOf(n);
+							updateOperation.andExpect(jsonPath(key).value(String.valueOf(fields.get(key))));
+						} catch (Exception e) {
+							e.printStackTrace();
+						}
+					}
+			);
+		}
+	}
+
+	/**
+	 * DELETE operation on User by user identifier
+	 * </p>
+	 * @param id     - user identifier
+	 * @param status - expected http status
+	 * @throws Exception on fails
+	 */
+	private void deleteUser(String id, ResultMatcher status) throws Exception {
 		String deletePath = REQUEST_PATH + "/" + id;
 		mockMvc.perform(delete(deletePath))
-				.andExpect(status().isOk())
+				.andExpect(status)
 				.andDo(print());
 	}
 }
